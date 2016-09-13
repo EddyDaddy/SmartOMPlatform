319b2550f6e555cda277708322936a43e45c80ca
zhanghe1990 <498018229@qq.com>
2016-09-13 10:20:54 +0800

    注册页面

--- JS/Register.js
+++ JS/Register.js
@@ -9,25 +9,29 @@ import {
     Image,
     TouchableNativeFeedback,
     TouchableHighlight,
+    ToastAndroid,
     Navigator,
     DeviceEventEmitter,
 } from 'react-native';
 import Dimensions from 'Dimensions';
 var screenWidth = Dimensions.get('window').width;
 var screenHeight = Dimensions.get('window').height;
+var TouchableElement = TouchableHighlight;
 class Register extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             userName: '',
-            erificationCodev: '',
+            verificationCode: '',
             passWord1: '',
             passWord2: '',
         };
     }
 
     componentDidMount() {
-
+        if (Platform.OS === 'android') {
+            TouchableElement = TouchableNativeFeedback;
+        }
     }
 
     getLoginUI() {
@@ -35,36 +39,62 @@ class Register extends React.Component {
             <View style={styles.root}>
                 <Image source={require('./img/bg.png')}
                        style={{width: screenWidth, height: screenHeight}}>
-                    <View style={styles.viewCenter}>
+                    <View style={styles.root}>
                         <Image source={require('./img/name.png')}
                                style={{marginTop: screenWidth/4.8, width: screenWidth/1.8, height: screenWidth/18}}></Image>
-                    </View>
-                    <View style={styles.viewCenter}>
                         <Image
                             source={require('./img/logo_img.png')}
                             style={{marginTop: screenWidth/18, width: screenWidth/3.86, height: screenWidth/3.86}}
                         />
-                    </View>
-                    <View style={styles.viewCenter}>
                         <View style={styles.borderView}>
                             <TextInput style={styles.textInput}
                                        onChangeText={(name) => this.setState({name})}
                                        value={this.state.name}
+                                       defaultValue="请输入您的手机号"
                             />
                         </View>
-                    </View>
 
-                    <View style={{width: screenWidth, marginTop: screenWidth/36, alignItems: 'center', flexDirection: 'row',
+                        <View style={{width: screenWidth/1.5, height: screenWidth / 9, marginTop: screenWidth/36, alignItems: 'center', flexDirection: 'row',
                         }}>
-                        <View style={styles.borderViewshort}>
+                            <View style={styles.borderViewshort}>
+                                <TextInput style={styles.textInput}
+                                           onChangeText={(verificationCode) => this.setState({verificationCode})}
+                                           value={this.state.verificationCode}
+                                           defaultValue="请填写验证码"
+                                />
+                            </View>
+                            <View style={{marginLeft: screenWidth/30, flex: 1, height: screenWidth/9,
+                             borderRadius: 6, alignItems: 'center', justifyContent: 'center', backgroundColor: '#ffd57d'}}>
+                                <TouchableElement onPress={()=>ToastAndroid.show('点击获取验证码', 0.05)}>
+                                    <Text style={{flex: 1}}>
+                                        验证码
+                                    </Text>
+                                </TouchableElement>
+                            </View>
+                        </View>
+                        <View style={styles.borderViewCommon}>
                             <TextInput style={styles.textInput}
-                                       onChangeText={(verificationCode) => this.setState({verificationCode})}
-                                       value={this.state.verificationCode}
+                                       onChangeText={(passWord1) => this.setState({passWord1})}
+                                       value={this.state.passWord1}
+                                       defaultValue="请设置密码"
                             />
                         </View>
-                        
-                    </View>
+                        <View style={styles.borderViewCommon}>
+                            <TextInput style={styles.textInput}
+                                       onChangeText={(passWord2) => this.setState({passWord2})}
+                                       value={this.state.passWord2}
+                                       defaultValue="请再次输入密码"
+                            />
+                        </View>
 
+                        <View style={{width: screenWidth/1.5, height: screenWidth/9, marginTop: screenWidth/36, borderRadius: 6, alignItems: 'center', justifyContent: 'center', backgroundColor: '#ffd57d'}}>
+                            <TouchableElement onPress={()=>ToastAndroid.show('点击登录', 0.05)}>
+                                <Text style={{flex: 1}}>
+                                    登录
+                                </Text>
+                            </TouchableElement>
+                        </View>
+                    </View>
                 </Image>
 
             </View>
@@ -84,7 +114,7 @@ class Register extends React.Component {
 
 var styles = StyleSheet.create({
         root: {
-            flex: 1
+            flex: 1, alignItems: 'center'
         },
         viewCenter: {
             width: screenWidth,
@@ -98,17 +128,26 @@ var styles = StyleSheet.create({
             backgroundColor: '#6ab5ba',
 
         },
-        borderViewshort: {
-            width: screenWidth / 2.3,
+        borderViewCommon: {
+            width: screenWidth / 1.5,
             height: screenWidth / 9,
             marginTop: screenWidth / 36,
             borderRadius: 6,
             backgroundColor: '#6ab5ba',
 
         },
+        borderViewshort: {
+            width: screenWidth / 2.3,
+            height: screenWidth / 9,
+            borderRadius: 6,
+            backgroundColor: '#6ab5ba',
+
+        },
         textInput: {
             flex: 1,
-            backgroundColor: '#00000000'
+            backgroundColor: '#00000000',
+            color: 'white',
+            paddingLeft: screenWidth/36
         }
     })
     ;

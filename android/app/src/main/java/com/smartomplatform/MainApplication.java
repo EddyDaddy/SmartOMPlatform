baf5ccc791059b04d5544386b38424fc28785405
EddyDaddy <lile_paul@163.com>
2016-09-22 22:45:23 +0800

    百度地图

--- android/app/src/main/java/com/smartomplatform/MainApplication.java
+++ android/app/src/main/java/com/smartomplatform/MainApplication.java
@@ -1,17 +1,18 @@
 package com.smartomplatform;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.imagepicker.ImagePickerPackage;
+
+import org.lovebing.reactnative.baidumap.BaiduMapPackage;
 
 import java.util.Arrays;
 import java.util.List;
-import com.imagepicker.ImagePickerPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new ImagePickerPackage()
+          new ImagePickerPackage(),
+          new BaiduMapPackage(getApplicationContext())
       );
     }
   };

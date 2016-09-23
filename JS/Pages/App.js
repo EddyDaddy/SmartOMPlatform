9e0aaac5a7c7f3caaac674cd57d961b40fdc8bcb
zhanghe1990 <498018229@qq.com>
2016-09-23 10:02:33 +0800

    App信鸽注册

--- JS/Pages/App.js
+++ JS/Pages/App.js
@@ -3,13 +3,13 @@
  */
 import React, {Component} from 'react';
 import {
+    Platform,
     Navigator,
     BackAndroid,
     View,
     StatusBar
 } from 'react-native';
 import login from './Login';
-import testMap from './testMap';
 var _navigator = null;
 //禁止左滑返回
 const NoBackSwipe = {
@@ -19,22 +19,102 @@ const NoBackSwipe = {
     }
 };
 class App extends Component {
+    // 构造
     constructor(props) {
         super(props);
+        // 初始状态
+        this.state = {
+            event: '',
+            eventArgs: null,
+            badgeNum: 0,
+            localUserInfo: {id: 1},
+            devToken: '',
+        };
     }
 
+    componentDidMount() {
+        XG.enableDebug(true);
+        console.log(XG.allEvents());
+        var registerHolder = XG.addEventListener('register', devToken => {
+            this.setState({
+                event: 'register',
+                eventArgs: JSON.stringify(devToken),
+                devToken,
+            });
+        });
 
-    render() {
-        return (
-            <View style={{flex:1}}>
-                <StatusBar
-                    translucent={true}
-                    backgroundColor="rgba(0, 0, 0, 0.2)"
-                    barStyle="light-content"
-                />
-                <Navigator
-                    initialRoute={{name:"login",component:login}}
-                    renderScene={
+        if (!registerHolder) console.log('Fail to add event to handle register');
+
+        var errorHolder = XG.addEventListener('error', err => {
+            this.setState({
+                event: 'error',
+                eventArgs: JSON.stringify(err)
+            });
+        });
+
+        if (!errorHolder) console.log('Fail to add event to handle error');
+
+        var remoteHolder = XG.addEventListener('notification', xgInstance => {
+            this.setState({
+                event: 'notification',
+                eventArgs: JSON.stringify(xgInstance)
+            });
+        });
+
+        if (!remoteHolder)
+            console.log('Fail to add event to handle remote notification');
+
+        var localHolder = XG.addEventListener('localNotification', xgInstance => {
+            console.log(xgInstance);
+            this.setState({
+                event: 'localNotification',
+                eventArgs: JSON.stringify(xgInstance)
+            });
+        });
+
+        if (!localHolder) console.log('Fail to add event to local notification');
+
+        this.setState({
+            eventsHolder: [
+                registerHolder,
+                errorHolder,
+                remoteHolder,
+                localHolder
+            ]
+        });
+
+        // Your accessId as number and your accessKey
+        XG.setCredential(2100230682, 'ANF2QS47Q44W');
+        XG.register('SampleTester');
+        if (Platform.OS === 'ios') {
+        }
+        XG.getApplicationIconBadgeNumber()
+            .then(badgeNum => {
+                this.setState({badgeNum});
+            })
+            .then(() => XG.checkPermissions())
+            .then(data => console.log(data));
+    }
+}
+
+componentWillUnmount()
+{
+    this.state.eventsHolder.filter(h => !!h).forEach(holder => holder.remove());
+}
+
+
+render()
+{
+    return (
+        <View style={{flex:1}}>
+            <StatusBar
+                translucent={true}
+                backgroundColor="rgba(0, 0, 0, 0.2)"
+                barStyle="light-content"
+            />
+            <Navigator
+                initialRoute={{name:"login",component:login}}
+                renderScene={
                         (route,navigator) =>{
                             _navigator = navigator;
                             let Component = route.component;
@@ -42,11 +122,11 @@ class App extends Component {
                             return <Component {...route.params} navigator={navigator} />
                         }
                     }
-                    configureScene={() => {return NoBackSwipe}}
-                />
-            </View>
-        );
-    }
+                configureScene={() => {return NoBackSwipe}}
+            />
+        </View>
+    );
+
 }
 
 BackAndroid.addEventListener("hardwareBackPress", ()=> {

37356bccd3880d25a580db5cb7beb2e6a8164338
jianweizhou <jianwei.zhou@aliyun.com>
2016-09-26 15:42:04 +0800

    图片压缩

--- android/app/src/main/java/com/smartomplatform/MainApplication.java
+++ android/app/src/main/java/com/smartomplatform/MainApplication.java
@@ -3,6 +3,7 @@ package com.smartomplatform;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImageResizerPackage(),
           new ImagePickerPackage(),
           new BaiduMapPackage(getApplicationContext()),
           new TencentXGPackage(),

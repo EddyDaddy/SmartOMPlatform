3eaa017f0db45035420aa22326e164864a2d2147
EddyDaddy <lile_paul@163.com>
2017-09-27 17:39:45 +0800

    谁谁谁

--- JS/Pages/MapWeb.js
+++ JS/Pages/MapWeb.js
@@ -6,18 +6,40 @@ import { WebView,View,StyleSheet } from 'react-native';
 import * as urls from '../Utils/Request.js'
 import Util from '../Utils/Utils.js'
 import Toolbar from '../Utils/ToolBar.js';
+import storge from '../Utils/Storage';
 var screenWidth = Util.size.width;
 var screenHeight = Util.size.height;
 
 var BGWASH = 'rgba(255,255,255,0.8)';
+var repairUserPhone;
+var userToken;
 
 class MapWeb extends Component {
+	
+	// 构造
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {
+            url: ''
+        }
+    }
+	
+	componentWillMount() {
+        const {navigator} = this.props;
+        storge.get('loginInfo').then((result) => {
+			this.setState({
+                        url: urls.MAP_URL+"?repairUserPhone="+result[0]+"&userToken="+result[1],
+                    })
+        });
+    }
+	
   render() {
     return (
 		<WebView
 		  style={{marginTop:  Util.pxToHeight(76)}}
           automaticallyAdjustContentInsets={false}
-          source={{uri: urls.MAP_URL}}
+          source={{uri: this.state.url}}
           javaScriptEnabled={true}
           domStorageEnabled={true}
           decelerationRate="normal"
--- JS/Pages/ReportWeb.js
+++ JS/Pages/ReportWeb.js
@@ -2,19 +2,44 @@
  * Created by demon on 2016/9/14.
  */
 import React, { Component } from 'react';
-import { WebView,View } from 'react-native';
+import { WebView,View,StyleSheet } from 'react-native';
 import * as urls from '../Utils/Request.js'
 import Util from '../Utils/Utils.js'
+import Toolbar from '../Utils/ToolBar.js';
+import storge from '../Utils/Storage';
+var screenWidth = Util.size.width;
+var screenHeight = Util.size.height;
 
 var BGWASH = 'rgba(255,255,255,0.8)';
+var repairUserPhone;
+var userToken;
 
 class ReportWeb extends Component {
-    render() {
+	
+	// 构造
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {
+            url: ''
+        }
+    }
+	
+	componentWillMount() {
+        const {navigator} = this.props;
+        storge.get('loginInfo').then((result) => {
+			this.setState({
+                        url: urls.REPORT_FORMS+"?repairUserPhone="+result[0]+"&userToken="+result[1],
+                    })
+        });
+    }
+	
+  render() {
     return (
 		<WebView
 		  style={{marginTop:  Util.pxToHeight(76)}}
           automaticallyAdjustContentInsets={false}
-          source={{uri: urls.REPORT_FORMS}}
+          source={{uri: this.state.url}}
           javaScriptEnabled={true}
           domStorageEnabled={true}
           decelerationRate="normal"
@@ -23,5 +48,4 @@ class ReportWeb extends Component {
     );
   }
 }
-
 export default ReportWeb;
